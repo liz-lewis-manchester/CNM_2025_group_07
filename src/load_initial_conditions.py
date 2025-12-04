2ca779f1d4bb5b873c135f69ce02fc9938473c86
EMyles88 <edward.myles@student.manchester.ac.uk>
2025-12-04 16:21:07 +0000

    Update load_initial_conditions.py

--- src/load_initial_conditions.py
+++ src/load_initial_conditions.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def load_initial_conditions(csv_path, x_model):
-  df = pd.read_csv(csv_path) 
+  df = pd.read_csv(csv_path, names=["dist", "conc"], header=0)
   #all rows column 0 become the distance column, and all rows in column 1 become the concentration coulmn
 
   distances = df.iloc[:,0].values
