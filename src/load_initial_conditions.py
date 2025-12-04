cd0a91f755a1009c6fe839fbee4ab57b36619086
EMyles88 <edward.myles@student.manchester.ac.uk>
2025-12-04 16:40:49 +0000

    Advenction_model.py
    Basic model setup alongside initial conditions

--- src/load_initial_conditions.py
+++ src/load_initial_conditions.py
@@ -9,3 +9,19 @@ def load_initial_conditions(csv_path, x_model):
   concentration = df.iloc[:,1].values
   theta0 = np.interp(x_model, distances, concentration)
   return theta0
+#Time and Space domain
+L = 3.0
+T = 300.0
+#step sizes
+dx = 0.1
+dt = 0.5
+#No. intervals
+Nx = int(L / dx)
+Nt = int(T / dt)
+#Space grid:
+x_model = np.linspace(0, L, Nx +1)
+#intital conditions
+theta = load_initial_conditions( "initial_conditions.csv", x_model)
+print("No. grid points:", len(x_model))
+print ("length of theta:", len(theta))
+print("First comple of theta values:", theta[:5])
